cfb215f404c9da25f404bb1f3c18a7ee07267144
Cyril Gavala <cyril.gavala@dazn.com>
2020-12-22 12:53:28 +0100

    Introducing version 3.1, which contains new endpoint for daily deaths statistics.

--- routes/seriesRoutes.js
+++ routes/seriesRoutes.js
@@ -57,4 +57,23 @@ router.get('/testsDaily', async (req, res) => {
     });
 })
 
+router.get('/deathsDaily', async (req, res) => {
+    console.log('%s INFO Recieved request for daily deaths series', new Date().toISOString())
+    DailyData.find({}, 'deaths date -_id', (err, data) => {
+        if (err) res.status(500).send(err);
+        else {
+            let result = []
+            for (let i = 0; i < data.length - 1; i++) {
+                let current = data[i + 1]
+                let previous = data[i]
+                result.push({
+                    "deaths": current.deaths - previous.deaths,
+                    "date": current.date
+                })
+            }
+            res.send(result);
+        }
+    });
+})
+
 module.exports = router;
