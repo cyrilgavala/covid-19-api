f938dbc1df485dcd4b2e9b0256374b4c3c71f246
Cyril Gavala <cyril.gavala@dazn.com>
2021-03-21 21:07:19 +0100

    ---- New version 4.0.0 ---- Introducing date range for series endpoints

--- routes/seriesRoutes.js
+++ routes/seriesRoutes.js
@@ -2,80 +2,119 @@ const express = require('express');
 const router = express.Router();
 const DailyData = require('../models/dailyData')
 
-router.get('/data', async (req, res) => {
-    console.log('%s INFO Recieved request for Data series', new Date().toISOString())
-    DailyData.find({}, 'confirmed recovered active date -_id', (err, data) => {
-        if (err) res.status(500).send(err);
-        else res.send(data);
-    });
-})
+/** @namespace req.query.startDate **/
+/** @namespace req.query.endDate **/
+//eslint-disable-express-serve-static-core
+
+function evaluateDates(startDate, endDate) {
+    if (startDate === null || startDate === undefined) {
+        console.error("%s ERROR Missing parameter 'startDate'", new Date().toISOString());
+        return "Missing parameter 'startDate'";
+    } else if (endDate === null || endDate === undefined) {
+        console.error("%s ERROR Missing parameter 'endDate'", new Date().toISOString());
+        return "Missing parameter 'endDate'";
+    } else if (new Date(startDate) >= new Date(endDate)) {
+        console.error("%s ERROR Invalid date range", new Date().toISOString())
+        return "Invalid date range. End date must be greater than start date";
+    } else {
+        return ""
+    }
+}
 
 router.get('/deaths', async (req, res) => {
-    console.log('%s INFO Recieved request for Deaths series', new Date().toISOString())
-    DailyData.find({}, 'deaths date -_id', (err, data) => {
-        if (err) res.status(500).send(err);
-        else res.send(data);
-    });
+    console.log('%s INFO Recieved request for deaths series from %s to %s',
+        new Date().toISOString(), req.query.startDate, req.query.endDate)
+    const errorMsg = evaluateDates(req.query.startDate, req.query.endDate)
+    if (errorMsg !== "") {
+        res.status(400).send({error: errorMsg});
+    } else {
+        DailyData.find({"date": {$gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)}},
+            'deaths date -_id', (err, data) => {
+            if (err) res.status(500).send(err);
+            else res.send(data);
+        })
+    }
 })
 
 router.get('/positivePercentage', async (req, res) => {
-    console.log('%s INFO Recieved request for Positive percentage series', new Date().toISOString())
-    DailyData.find({}, 'numberOfTests confirmed date -_id', (err, data) => {
-        if (err) res.status(500).send(err);
-        else {
-            let result = []
-            for (let i = 0; i < data.length - 1; i++) {
-                let current = data[i + 1]
-                let previous = data[i]
-                let percentage = (current.numberOfTests - previous.numberOfTests) > 0 ?
-                    ((current.confirmed - previous.confirmed) / (current.numberOfTests - previous.numberOfTests) * 100).toFixed(2) : 0;
-                result.push({
-                    "percentage": percentage,
-                    "date": current.date
-                })
+    console.log('%s INFO Recieved request for positive percentage series from %s to %s',
+        new Date().toISOString(), req.query.startDate, req.query.endDate)
+    const errorMsg = evaluateDates(req.query.startDate, req.query.endDate)
+    if (errorMsg !== "") {
+        res.status(400).send({error: errorMsg});
+    } else {
+        DailyData.find({"date": {$gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)}},
+            'numberOfTests confirmed date -_id', (err, data) => {
+            if (err) res.status(500).send(err);
+            else {
+                let result = []
+                for (let i = 0; i < data.length - 1; i++) {
+                    let current = data[i + 1]
+                    let previous = data[i]
+                    let percentage = (current.numberOfTests - previous.numberOfTests) > 0 ?
+                        ((current.confirmed - previous.confirmed) / (current.numberOfTests - previous.numberOfTests) * 100).toFixed(2) : 0;
+                    result.push({
+                        "percentage": percentage,
+                        "date": current.date
+                    })
+                }
+                res.send(result);
             }
-            res.send(result);
-        }
-    });
+        });
+    }
 })
 
 router.get('/testsDaily', async (req, res) => {
-    console.log('%s INFO Recieved request for No. of tests series', new Date().toISOString())
-    DailyData.find({}, 'numberOfTests confirmed date -_id', (err, data) => {
-        if (err) res.status(500).send(err);
-        else {
-            let result = []
-            for (let i = 0; i < data.length - 1; i++) {
-                let current = data[i + 1]
-                let previous = data[i]
-                result.push({
-                    "tests": current.numberOfTests - previous.numberOfTests,
-                    "confirmed": current.confirmed - previous.confirmed,
-                    "date": current.date
-                })
+    console.log('%s INFO Recieved request for daily tests series from %s to %s',
+        new Date().toISOString(), req.query.startDate, req.query.endDate)
+    const errorMsg = evaluateDates(req.query.startDate, req.query.endDate)
+    if (errorMsg !== "") {
+        res.status(400).send({error: errorMsg});
+    } else {
+        DailyData.find({"date": {$gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)}},
+            'numberOfTests confirmed date -_id', (err, data) => {
+            if (err) res.status(500).send(err);
+            else {
+                let result = []
+                for (let i = 0; i < data.length - 1; i++) {
+                    let current = data[i + 1]
+                    let previous = data[i]
+                    result.push({
+                        "tests": current.numberOfTests - previous.numberOfTests,
+                        "confirmed": current.confirmed - previous.confirmed,
+                        "date": current.date
+                    })
+                }
+                res.send(result);
             }
-            res.send(result);
-        }
-    });
+        });
+    }
 })
 
 router.get('/deathsDaily', async (req, res) => {
-    console.log('%s INFO Recieved request for daily deaths series', new Date().toISOString())
-    DailyData.find({}, 'deaths date -_id', (err, data) => {
-        if (err) res.status(500).send(err);
-        else {
-            let result = []
-            for (let i = 0; i < data.length - 1; i++) {
-                let current = data[i + 1]
-                let previous = data[i]
-                result.push({
-                    "deaths": current.deaths - previous.deaths,
-                    "date": current.date
-                })
+    console.log('%s INFO Recieved request for daily deaths series from %s to %s',
+        new Date().toISOString(), req.query.startDate, req.query.endDate)
+    const errorMsg = evaluateDates(req.query.startDate, req.query.endDate)
+    if (errorMsg !== "") {
+        res.status(400).send({error: errorMsg});
+    } else {
+        DailyData.find({"date": {$gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)}},
+            'deaths date -_id', (err, data) => {
+            if (err) res.status(500).send(err);
+            else {
+                let result = []
+                for (let i = 0; i < data.length - 1; i++) {
+                    let current = data[i + 1]
+                    let previous = data[i]
+                    result.push({
+                        "deaths": current.deaths - previous.deaths,
+                        "date": current.date
+                    })
+                }
+                res.send(result);
             }
-            res.send(result);
-        }
-    });
+        });
+    }
 })
 
 module.exports = router;
--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -15,17 +15,4 @@ router.get('/day', async (req, res) => {
     }
 })
 
-router.get('/days', async (req, res) => {
-    console.log("%s INFO Recieved get daily data request since %s", new Date().toISOString(), req.query.date);
-    if (req.query.date === null || req.query.date === undefined) {
-        console.error("Missing parameter 'date'");
-        res.status(400).send({error: "Missing parameter 'date'"});
-    } else {
-        DailyData.find({"date": {$gte: new Date(req.query.date)}}, '-_id',{}, (err, data) => {
-            if (err) res.status(500).send(err);
-            else res.send(data);
-        })
-    }
-})
-
 module.exports = router
